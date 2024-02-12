caff756a8acca87a644282b98e7ca760480c5bd8
Enrique Garcia Illera <egaillera@gmail.com>
2024-02-12 15:19:30 +0100

    [FEAT] Modify prompt to add several recommendations and get the entity that issues the fund

--- chains.py
+++ chains.py
@@ -87,7 +87,8 @@ def prepare_setup():
     template = """You are a financial assistant for question-answering tasks related to investment funds. 
                 Use the following pieces of retrieved context to answer the question. 
                 The answer should be always in Spanish.
-                If you found a fund, explain its features t a bit
+                Try to provide three recommendations adapted to the request
+                If you found a fund, explain its features a bit and say which bank issues it.
                 If you don't know the answer, just say that you don't know. 
                 Use six sentences maximum and keep the answer concise.
                 Question: {question} 
